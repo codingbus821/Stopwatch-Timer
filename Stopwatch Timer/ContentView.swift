d73fa4ecfb57c4c31fe1afd45cc3c7a355fe86db
kimbyeongsoo <kbsu147@naver.com>
2020-09-23 00:31:06 +0900

    Initial Commit

---
+++ Stopwatch Timer/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Stopwatch Timer
+//
+//  Created by 김병수 on 2020/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
